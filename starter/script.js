9dd8e2249a680cd09614eb01337804484e940856
syed <novelcode0@gmail.com>
2021-05-05 17:00:16 +0530

    Lec 145 Creating DOM Elements

--- starter/script.js
+++ starter/script.js
@@ -59,9 +59,31 @@ const inputTransferTo = document.querySelector('.form__input--to');
 const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector(
-  '.form__input--pin'
-); /*
+const inputClosePin = document.querySelector('.form__input--pin');
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">
+        ${i + 1}
+        ${type}
+      </div>
+      <div class="movements__value">${mov}</div>
+    </div>
+  `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
+// Lec 145 Creating DOM Elements ^
+// Lec 144 PROJECT: "Bankist" App (intro about project)
+
+/*
 // Lec 143 forEach with Maps and Sets
 // Map
 const currencies = new Map([
